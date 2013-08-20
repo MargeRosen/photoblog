87b918ff062090451bb135477cc6ecba5b0e6e21
Marge Rosen <marge@masterclassu.com>
2013-08-20 15:00:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Photo blog
+
+Create your own photo blog.  Upload your images and tell your story.
+
+
+
+* Services (job queues, cache servers, search engines, etc.)
+
+#### To set-up this app in development, run these commands:
+
+```
+bundle
+rake db:migrate
+rails s
+```
+T
+his Ruby version 2.0.0, Rails 4.0.0
+
+### License:
+
+MIT http://marge.mit-license.org
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photoblog::Application.config.secret_key_base = 'b89cda4ca425d5763304df53e05a85d8fe2ac1144485acf118f38123a59c543ee13c46d30e8e82e38cfd0c72ce30fe501093ce839a63c282c6f85782188dc23d'
